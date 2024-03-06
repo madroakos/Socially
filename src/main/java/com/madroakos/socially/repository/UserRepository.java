384bfe855f0fd9853e9db5de8fe54bdccf2ce890
madroakos <madroboy@protonmail.com>
2024-03-07 00:33:23 +0100

    Initial commit
    Set up webserver and Users class and repository

--- src/main/java/com/madroakos/socially/repository/UserRepository.java
+++ src/main/java/com/madroakos/socially/repository/UserRepository.java
@@ -4,5 +4,4 @@ import com.madroakos.socially.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // You can add custom query methods here if needed
 }
