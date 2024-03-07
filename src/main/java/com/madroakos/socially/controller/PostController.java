aaf442152b55f94b9218131307d9c7e268406868
madroakos <madroboy@protonmail.com>
2024-03-07 11:08:16 +0100

    Add ability to retrieve posts by username

--- src/main/java/com/madroakos/socially/controller/PostController.java
+++ src/main/java/com/madroakos/socially/controller/PostController.java
@@ -3,10 +3,7 @@ package com.madroakos.socially.controller;
 import com.madroakos.socially.model.Post;
 import com.madroakos.socially.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,10 +16,15 @@ public class PostController {
         this.postRepository = postRepository;
     }
 
-    @GetMapping("/post")
-    public List<Post> getPosts() {
-        return postRepository.findAll();
+    @GetMapping("/post") //Request without username should be removed later
+    public List<Post> getPosts(@RequestParam(required = false) String username) {
+        if (username != null) {
+            return postRepository.findByUsername(username);
+        } else {
+            return postRepository.findAll();
+        }
     }
+
 
     @PostMapping("/post")
     public void submitPost(@RequestBody Post post) {
--- src/main/java/com/madroakos/socially/repository/PostRepository.java
+++ src/main/java/com/madroakos/socially/repository/PostRepository.java
@@ -3,5 +3,9 @@ package com.madroakos.socially.repository;
 import com.madroakos.socially.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByUsername(String username);
+
 }
