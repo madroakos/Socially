a433dd6640bdb196df065f841dec13b85eb91004
madroakos <madroboy@protonmail.com>
2024-03-13 22:19:02 +0100

    refactor: Change page name to frontPage

--- src/main/java/com/madroakos/socially/controller/PageController.java
+++ src/main/java/com/madroakos/socially/controller/PageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class PageController {
 
     @GetMapping("/")
-    public String welcome() {
-        return "welcome";
+    public String frontPage() {
+        return "frontPage";
     }
 }
