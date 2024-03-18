063f1418abb82a42cb47009ab4c30fbfe67f6317
madroakos <madroboy@protonmail.com>
2024-03-18 15:48:44 +0100

    refactor: Simplify code

---
+++ src/main/java/com/madroakos/socially/dto/PostContent.java
@@ -0,0 +1,11 @@
+package com.madroakos.socially.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class PostContent {
+    private String postContent;
+
+}
--- src/main/java/com/madroakos/socially/model/Post.java
+++ src/main/java/com/madroakos/socially/model/Post.java
@@ -1,9 +1,13 @@
 package com.madroakos.socially.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "app_post")
 public class Post {
@@ -13,45 +17,4 @@ public class Post {
     private String username;
     private String postContent;
     private LocalDateTime timeSubmitted;
-
-    public Post() {
-    }
-
-    public Post(String username, String postContent, LocalDateTime timeSubmitted) {
-        this.username = username;
-        this.postContent = postContent;
-        this.timeSubmitted = timeSubmitted;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPostContent() {
-        return postContent;
-    }
-
-    public void setPostContent(String postContent) {
-        this.postContent = postContent;
-    }
-
-    public LocalDateTime getTimeSubmitted() {
-        return timeSubmitted;
-    }
-
-    public void setTimeSubmitted(LocalDateTime timeSubmitted) {
-        this.timeSubmitted = timeSubmitted;
-    }
 }
